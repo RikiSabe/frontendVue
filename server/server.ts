a3f81fec80c358702d10d5643e12da13aa645560
riki <riki.cam.flo@gmail.com>
2024-12-06 18:16:03 -0400

    cambio de url

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
 export enum server {
-    HOST= 'http://localhost:5000',
-    HOSTWS= 'localhost:5000'
+    HOST= 'https://backendgo.dev.dtt.tja.ucb.edu.bo:5000',
+    HOSTWS= 'https://backendgo.dev.dtt.tja.ucb.edu.bo:5000'
 }
