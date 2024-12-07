2ec229c9a9c2ba70ba7e69bca6c70ad47c64c723
riki <riki.cam.flo@gmail.com>
2024-12-06 21:03:17 -0400

    last

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,6 @@
 export enum server {
-    HOST= 'https://backendgo.dev.dtt.tja.ucb.edu.bo',
-    HOSTWS= 'https://backendgo.dev.dtt.tja.ucb.edu.bo'
+    HOST= 'http://localhost:5000',
+    HOSTWS= 'localhost:5000'
+    // HOST= 'https://backendgo.dev.dtt.tja.ucb.edu.bo',
+    // HOSTWS= 'https://backendgo.dev.dtt.tja.ucb.edu.bo'
 }
